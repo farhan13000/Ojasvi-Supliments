45115bff5cc97a54c3e42952ae5c92f20401982c
Md Farhan <mdfstar3000@gmail.com>
2026-09-02 22:52:02 +0530

    fix(seo): remove prerender plugin to resolve Vercel deployment issues

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import Sitemap from 'vite-plugin-sitemap'
-import prerender from '@prerenderer/rollup-plugin'
-import puppeteer from '@prerenderer/renderer-puppeteer'
 import { defineConfig } from 'vite'
 
 const dynamicRoutes = [
   '/products/strength-plus'
 ]
-
-const staticRoutes = ['/', '/products', '/about', '/contact', '/faq']
-const allRoutes = [...staticRoutes, ...dynamicRoutes]
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,13 +16,6 @@ export default defineConfig({
       hostname: 'https://ojashvisupplements.vercel.app',
       dynamicRoutes,
       exclude: ['/404']
-    }),
-    prerender({
-      routes: allRoutes,
-      renderer: '@prerenderer/renderer-puppeteer',
-      server: {
-        port: 3000,
-      }
     })
   ],
   build: {
